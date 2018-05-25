137b1462f317229bd7aad1d877991191ce4b5580
kgori <kevin.c.gori@gmail.com>
2018-05-25 17:49:24 +0100

    Added fuzzy matching

--- pcr_scanner.py
+++ pcr_scanner.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 import contextlib
 import logging
+import fuzzysearch
 logging.basicConfig(format='%(levelname)s:%(asctime)s:%(message)s',
                     level=logging.DEBUG)
 
@@ -65,19 +66,19 @@ def read_primers(filename):
         for line in fl:
             p1, p2 = line.rstrip().split()
             primers.append((p1, p2))
-    return primers
+    return primers, [(revcomp(p1), revcomp(p2)) for (p1, p2) in primers]
 
 
-def build_regex(primers_list):
-    import re
-    rgxs = [re.compile(r'(.*){}(.+){}(.*)'.format(primer[1], primer[0]))
-            for primer in primers_list]
-    rgxs.extend(
-            re.compile(r'(.*){}(.+){}(.*)'.format(revcomp(primer[0]),
-                       revcomp(primer[1])))
-            for primer in primers_list)
-    multirgx = re.compile('|'.join('(?:{0})'.format(x.pattern) for x in rgxs))
-    return multirgx
+def fuzzy_search(string, primer_pairs):
+    """
+    Search `string` for both primer sequences
+    """
+    for (i, primer_pair) in enumerate(primer_pairs):
+        search1 = fuzzysearch.find_near_matches(primer_pair[0], string, max_l_dist=2)
+        if search1:
+            search2 = fuzzysearch.find_near_matches(primer_pair[1], string, max_l_dist=2)
+            if search2:
+                return i, search1[0], search2[0]
 
 
 if __name__ == '__main__':
@@ -90,10 +91,9 @@ if __name__ == '__main__':
         logging.error('Primers file not found: {}'.format(args.primers))
         sys.exit()
     else:
-        primers = read_primers(args.primers)
+        primers, rev_primers = read_primers(args.primers)
         logging.info('Read primers from {}'.format(args.primers))
-        multirgx = build_regex(primers)
-        logging.info('Built regular expression')
+        
 
     # Work out if reads are coming from standard input or a file
     reads_filename = None
@@ -103,8 +103,8 @@ if __name__ == '__main__':
             logging.error('Reads file not found: {}'.format(reads_filename))
             sys.exit()
 
-    print('READ\tCHROM\tPOS\tPRIMERID\tPREEXTENSION\tEXTENSION\t'
-          'SEQUENCE\tLIGATION\tPOSTLIGATION\tUMI\tPRIMER.ORIENTATION\tREAD.ORIENTATION')
+    print('READ\tCHROM\tPOS\tPRIMERID\tPRELEFT\tLEFTPRIMER\t'
+          'SEQUENCE\tRIGHTPRIMER\tPOSTRIGHT\tUMI\tPRIMER.ORIENTATION\tREAD.ORIENTATION\tSEQ')
     try:
         with smart_filereader(reads_filename) as readsfile:
             lines_processed = 0
@@ -117,35 +117,47 @@ if __name__ == '__main__':
                 pos = fields[3]
                 read_is_reverse = flag & 16 == 16
                 seq = fields[9]
-                search = multirgx.search(seq)
+                search = fuzzy_search(seq, primers)
                 if search:
-                    rgx_pos = match_position(search.groups())
-                    primer_pos = (rgx_pos // 3)  # Each regex has 3 capture groups -> original primer list is 3 times shorter
-                    primer_is_reverse = primer_pos >= len(primers)
-                    primer_pos = primer_pos % len(primers)  # Shift reverse-match position back to forward primer sequence
-                    extension = primers[primer_pos][0]
-                    ligation = primers[primer_pos][1]
-                    pre_extension, captured, post_ligation = search.groups()[rgx_pos:rgx_pos+3]
-
-                    if primer_is_reverse:
-                        umi = post_ligation[:5]
+                    primer_is_reverse = False
+                else:
+                    search = fuzzy_search(seq, rev_primers)
+                    if search:
+                        primer_is_reverse = True
+                if search:
+                    primer_pos, match1, match2 = search
+                    if (match1.start, match1.end) < (match2.start, match2.end):
+                        l_s, l_e, r_s, r_e = match1.start, match1.end, match2.start, match2.end
 
                     else:
-                        umi = pre_extension[-5:]
+                        l_s, l_e, r_s, r_e = match2.start, match2.end, match1.start, match1.end
+                    
+                    pre_left = seq[:l_s]
+                    left_primer = seq[l_s:l_e]
+                    captured = seq[l_e:r_s]
+                    right_primer = seq[r_s:r_e]
+                    post_right = seq[r_e:]
+                    
+                    if primer_is_reverse:
+                        umi = post_right[:5]
+
+                    else:
+                        umi = pre_left[-5:]
 
                     # Print the read name, index of matching primer (0-based), the matched sequences, and their orientation.
                     # Orientation note: output is displayed in same orientation as primer sequence,
                     # but if orientation is marked '-' the read sequence in the BAM is from the opposite strand
                     print(
-                        ('{read}\t{chrom}\t{pos}\t{primerid}\t{preextension}\t{extension}\t'
-                         '{sequence}\t{ligation}\t{postligation}\t{umi}\t'
-                         '{primerorientation}\t{readorientation}')
+                        ('{read}\t{chrom}\t{pos}\t{primerid}\t{preleft}\t{left}\t'
+                         '{sequence}\t{right}\t{postright}\t{umi}\t'
+                         '{primerorientation}\t{readorientation}\t{seq}')
                         .format(read=name, chrom=chrom, pos=pos, primerid=primer_pos,
-                                preextension=pre_extension, extension=extension,
-                                sequence=captured, ligation=ligation, umi=umi, postligation=post_ligation,
+                                preleft=pre_left, left=left_primer,
+                                sequence=captured, right=right_primer, umi=umi, postright=post_right,
                                 primerorientation=('-' if primer_is_reverse else '+'),
-                                readorientation=('-' if read_is_reverse else '+')))
-                if lines_processed % 100 == 0:
+                                readorientation=('-' if read_is_reverse else '+'),
+                                seq=seq))
+                if lines_processed % 1000 == 0:
                     logging.debug('Processed {} lines'.format(lines_processed))
     except (BrokenPipeError, KeyboardInterrupt) as err:
         logging.debug("Broken pipe")
